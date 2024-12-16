dc64bf4fb0891c42fe099bbdbedb61b0915ef9e1
mhurst <matthewhurst1791@gmail.com>
2024-12-15 22:22:07 -0500

    lab complete

--- src/components/Bookshelf.jsx
+++ src/components/Bookshelf.jsx
@@ -1,9 +1,7 @@
 import { useState } from "react"
 
 const Bookshelf = () => {
-    const [books, setBooks] = useState([
-        { title: '', author: '' }
-    ])
+    const [books, setBooks] = useState([])
     const [newBook, setNewBook] = useState([
         { title: '', author: '' }
     ])
@@ -13,13 +11,14 @@ const Bookshelf = () => {
     const handleSubmit = (event) => {
         // prevent default event behavior
         event.preventDefault()
-        setBooks({...books, newBook})
+        setBooks([...books, newBook])
         setNewBook({
           title: '', 
           author: ''
         })
     }
     return (
+        <>
         <div className="bookshelfDiv">
             <div className="formDiv">
                 <h3>Add a Book</h3>
@@ -27,7 +26,6 @@ const Bookshelf = () => {
                     <div>
                         <label htmlFor="title">Title:</label>
                         <input
-                            // type="text"
                             id="title" 
                             name="title" 
                             value={newBook.title}
@@ -37,7 +35,6 @@ const Bookshelf = () => {
                     <div>
                         <label htmlFor="author">Author:</label>
                         <input
-                            // type="text"
                             id="author" 
                             name="author" 
                             value={newBook.author}
@@ -49,17 +46,18 @@ const Bookshelf = () => {
                     </div>
                 </form>
             </div>
-            <div className="bookCardsDiv">
-                <ul>
-                    {books.map((book, index) => (
-                        <li key={index}>
-                            <h3>Title: {book.title}</h3>
-                            <h3>Author: {book.author}</h3>
-                        </li>
-                    ))}
-                </ul>
-            </div>
         </div>
+        <div>
+            <ul>
+                {books.map((book, index) => (
+                    <li key={index} className="bookCard">
+                        <h3>Title: {book.title}</h3>
+                        <h3>Author: {book.author}</h3>
+                    </li>
+                ))}
+            </ul>
+        </div>
+        </>
     )
 }
 
